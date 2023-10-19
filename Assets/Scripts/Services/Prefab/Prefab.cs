ba68277f14e6f6ee1863dd98b4f465fb52bb66d7
Mustafa Toprak <MToprak.L203@gmail.com>
2023-10-19 14:55:46 +0300

    Prefab Scripti daha okunabilir yaptım

--- Assets/Scripts/Services/Prefab/Prefab.cs
+++ Assets/Scripts/Services/Prefab/Prefab.cs
@@ -76,13 +76,17 @@ namespace Prefab
 
         public void Initialize()
         {
-            // ------------ Combine Mesh ------------
+            CombineMeshes();
+            ResizePrefab();
+            CreateRimMaterials();
+        }
 
+        private void CombineMeshes()
+        {
             // Get all mesh filters in the prefab.
             List<MeshFilter> meshFilters = GetComponentsInChildren<MeshFilter>().ToList();
             var rootMeshFilter = GetComponent<MeshFilter>();
-            if (rootMeshFilter) meshFilters.Add(rootMeshFilter);
-            
+            if (rootMeshFilter != null) meshFilters.Add(rootMeshFilter);
 
             // Create CombineInstance array to keep combine data
             CombineInstance[] combine = new CombineInstance[meshFilters.Count];
@@ -98,16 +102,20 @@ namespace Prefab
                     combine[i].mesh = meshFilter.sharedMesh;
                     combine[i].transform = meshFilter.transform.localToWorldMatrix;
                 }
-                else Debug.LogWarning("Mesh is not readable: " + meshFilter.name + " in " + gameObject.name);
+                else
+                {
+                    Debug.LogWarning("Mesh is not readable: " + meshFilter.name + " in " + gameObject.name);
+                }
             }
 
             _combinedMesh = new Mesh();
             _combinedMesh.CombineMeshes(combine, false, true, false);
+        }
 
+        private void ResizePrefab()
+        {
             // Get bounds of the combined mesh
             Bounds bounds = _combinedMesh.bounds;
-
-            // ------------ Resize Prefab ------------
 
             // Find the largest dimension (x, y, or z)
             float largestDimension = Mathf.Max(bounds.size.x, bounds.size.y, bounds.size.z);
@@ -117,17 +125,21 @@ namespace Prefab
 
             // Apply the scaling factor to the prefab
             transform.localScale = _scaleFactor * Vector3.one;
+        }
 
-
-            // ------------ Create Rim Materials ------------
-
+        private void CreateRimMaterials()
+        {
             List<MeshRenderer> meshRenderers = GetComponentsInChildren<MeshRenderer>().ToList();
             var rootRenderer = GetComponent<MeshRenderer>();
             if (rootRenderer != null) meshRenderers.Add(rootRenderer);
 
             _rimMaterials = new Material[meshRenderers.Count];
             Shader shader = Shader.Find("Custom/Ghost Rim");
-            for (int i = 0; i < meshRenderers.Count; i++) _rimMaterials[i] = new Material(shader);
+
+            for (int i = 0; i < meshRenderers.Count; i++)
+            {
+                _rimMaterials[i] = new Material(shader);
+            }
         }
 
         public GameObject CreateWrapper(Vector3 createPos)
@@ -137,21 +149,31 @@ namespace Prefab
 
             // Create wrapper
             GameObject wrapper = new GameObject("Wrapper_" + name);
-            wrapper.layer = LayerMask.NameToLayer("Draggable");
-            wrapper.transform.position = createPos;
-            wrapper.transform.localScale = _scaleFactor * Vector3.one;
-
-            // Make prefab child of the wrapper
-            wrapper.transform.SetParent(spawnedPrefab.transform.parent);
-            spawnedPrefab.transform.SetParent(wrapper.transform);
-            spawnedPrefab.transform.localPosition = Vector3.zero;
+            InitializeWrapper(wrapper, spawnedPrefab, createPos);
 
             // Add mesh components to the wrapper
             wrapper.AddComponent<MeshFilter>().mesh = _combinedMesh;
             wrapper.AddComponent<MeshRenderer>().materials = _rimMaterials;
 
-            // Create box collider
+            CreateBoxCollider(wrapper);
 
+            return wrapper;
+        }
+
+        private void InitializeWrapper(GameObject wrapper, GameObject spawnedPrefab, Vector3 createPos)
+        {
+            wrapper.layer = LayerMask.NameToLayer("Draggable");
+            wrapper.transform.position = createPos;
+            wrapper.transform.localScale = _scaleFactor * Vector3.one;
+
+            // Make prefab a child of the wrapper
+            wrapper.transform.SetParent(spawnedPrefab.transform.parent);
+            spawnedPrefab.transform.SetParent(wrapper.transform);
+            spawnedPrefab.transform.localPosition = Vector3.zero;
+        }
+
+        private void CreateBoxCollider(GameObject wrapper)
+        {
             // Get bounds of the combined mesh
             Bounds bounds = _combinedMesh.bounds;
 
@@ -160,8 +182,6 @@ namespace Prefab
             boxCollider.size = new Vector3(0.8f * bounds.size.x, bounds.size.y, 0.8f * bounds.size.z);
             boxCollider.center = bounds.center;
             boxCollider.isTrigger = true;
-
-            return wrapper;
         }
     }
 }
